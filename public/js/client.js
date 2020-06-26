c406c0536300ecc4d494e7b47f6656b511efae8a
blueblackzebra <sparshkasana99@gmail.com>
2020-06-26 21:31:04 +0530

    Doc comments for client side

--- public/js/client.js
+++ public/js/client.js
@@ -1,12 +1,13 @@
 const socket=io(':8443', {secure: true});
 
-let peerInfo = new Array();
+let peerInfo = new Array(); // Array of all WebRTC peers and their RTSP URI
 let RtcPeer;
-let count=0;
+let count=0; // Keeps track of number of video streams
 
 const $add1 =document.querySelector('#add1');
 const $div1 =document.querySelector('#div1');
 
+// Dynamic addition of HTML for new video streams
 $add1.addEventListener('click',() => {
     console.log('new fields added');
     $div1.insertAdjacentHTML('beforeend',`<video id="output-${count}" autoplay></video><br>
@@ -20,6 +21,7 @@ $add1.addEventListener('click',() => {
 
 let itemid,splitid;
 
+// Event delegation code for all streams, checks for 'submit', 'start' and 'stop'
 $div1.addEventListener('click',(event) => {
     
     itemid = event.target.id;
@@ -29,6 +31,7 @@ $div1.addEventListener('click',(event) => {
     buttonType=splitid[0]
     num=splitid[1]
     
+    // Store submitted URL
     if(buttonType=='submit'){
         console.log('submit registered');
         let currinput = document.querySelector(`#input-${num}`);
@@ -37,6 +40,7 @@ $div1.addEventListener('click',(event) => {
         currinput.value="";
     }
     
+    // Stop the stream and dispose the WebRTC peer
     else if(buttonType=='stop'){
         console.log('stop registered');
         let curroutput = document.querySelector(`#output-${num}`);
@@ -45,6 +49,7 @@ $div1.addEventListener('click',(event) => {
         peerInfo[num].rtcPeer = null;
     }
     
+    // Creates WebRTC peer for the URL and sends SDP offer
     else if(buttonType=='start'){
         console.log('start registered');
         let curroutput = document.querySelector(`#output-${num}`);
@@ -66,46 +71,27 @@ $div1.addEventListener('click',(event) => {
                 console.log(error)
             }
             socket.emit('sdpOffer',offer,peerInfo[num].url,num);
-        // console.log(offer)
         })
     }
 })
 
+// Passed into WebRTC peer constuctor, receives and emits ICE candidates
 function iceCandidate(candidate){
     socket.emit('initice', candidate)
 }
 
+// Receives and processes SDP answer from server-side
 socket.on('sdpAnswer', (answer, number) => {
     peerInfo[number].rtcPeer.processAnswer(answer)
     console.log("answer")
 })
 
+// Receives and adds ICE candidates
 socket.on('finalice', (candidate, number) => {
     peerInfo[number].rtcPeer.addIceCandidate(candidate);
-    // console.log('haha' + '\n' + candidate)
-    // setTimeout(() => {
-    //     RtcPeer.send(videostream);
-    // }, 2000);
     })
 
+// Listens for errors
 socket.on('problem', (message)=> {
     console.log(message);
 })
-
-// $mirror.addEventListener('click',() => {
-//     RtcPeer=kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly({
-//         remoteVideo: $video1,
-//         onicecandidate : iceCandidate
-//     }, function (error) {
-//         if (error){
-//             console.log(error)
-//         }
-//         // console.log("dne")
-//     }) 
-
-//     RtcPeer.generateOffer((error,offer)=> {
-//         // console.log(error)
-//         socket.emit('sdpOffer',offer);
-//         // console.log(offer)
-//     })
-// })
